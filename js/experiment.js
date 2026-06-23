884b8c0bc0d1bf1c6b6c5549f8b10a00db16ab47
hmhouse-jade <jade.misun.ryu@gmail.com>
2026-06-23 19:11:26 +0900

    Add files via upload

--- js/experiment.js
+++ js/experiment.js
@@ -1,51 +1,60 @@
-import { shuffle } from "./utils.js";
-import { positiveWords, negativeWords, positiveImages, negativeImages } from "./data.js";
+import {positiveWords, negativeWords, positiveImages, negativeImages} from "./data.js";
+import {shuffle} from "./utils.js";
 
-export let results=[];
-export let errorCount=0;
+export const practiceBlock = {
+  name:"연습",
+  leftLabel:"자음",
+  rightLabel:"모음",
+  trials:[
+    {type:"text",content:"ㄱ",correct:"left"},
+    {type:"text",content:"ㅏ",correct:"right"},
+    {type:"text",content:"ㄴ",correct:"left"},
+    {type:"text",content:"ㅓ",correct:"right"},
+    {type:"text",content:"ㄷ",correct:"left"},
+    {type:"text",content:"ㄹ",correct:"left"},
+    {type:"text",content:"ㅗ",correct:"right"},
+    {type:"text",content:"ㅣ",correct:"right"}
+  ]
+};
 
 export function createBlocks(){
 
-  const block1Trials=[];
-  const block2Trials=[];
+  const block1 = [];
+  const block2 = [];
 
-  positiveWords.forEach(word=>{
-    block1Trials.push({type:"text",content:word,correct:"left"});
+  positiveWords.forEach(w=>{
+    block1.push({type:"text",content:w,correct:"left"});
+    block2.push({type:"text",content:w,correct:"right"});
   });
 
-  negativeWords.forEach(word=>{
-    block1Trials.push({type:"text",content:word,correct:"right"});
-  });
-
-  positiveImages.forEach(img=>{
-    block1Trials.push({type:"image",content:img,correct:"left"});
-  });
-
-  negativeImages.forEach(img=>{
-    block1Trials.push({type:"image",content:img,correct:"right"});
-  });
-
-  positiveWords.forEach(word=>{
-    block2Trials.push({type:"text",content:word,correct:"right"});
-  });
-
-  negativeWords.forEach(word=>{
-    block2Trials.push({type:"text",content:word,correct:"left"});
+  negativeWords.forEach(w=>{
+    block1.push({type:"text",content:w,correct:"right"});
+    block2.push({type:"text",content:w,correct:"left"});
   });
 
   positiveImages.forEach(img=>{
-    block2Trials.push({type:"image",content:img,correct:"right"});
+    block1.push({type:"image",content:img,correct:"left"});
+    block2.push({type:"image",content:img,correct:"right"});
   });
 
   negativeImages.forEach(img=>{
-    block2Trials.push({type:"image",content:img,correct:"left"});
+    block1.push({type:"image",content:img,correct:"right"});
+    block2.push({type:"image",content:img,correct:"left"});
   });
-
-  shuffle(block1Trials);
-  shuffle(block2Trials);
 
   return [
-    {name:"본실험 1",leftLabel:"긍정",rightLabel:"부정",trials:block1Trials},
-    {name:"본실험 2",leftLabel:"부정",rightLabel:"긍정",trials:block2Trials}
+    practiceBlock,
+    {
+      name:"본실험 1",
+      leftLabel:"긍정",
+      rightLabel:"부정",
+      trials: shuffle(block1)
+    },
+    {
+      name:"본실험 2",
+      leftLabel:"부정",
+      rightLabel:"긍정",
+      trials: shuffle(block2)
+    }
   ];
 }
